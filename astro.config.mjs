ae3a4d6a355ad8e4b87ce7f0c8f16a70956a2093
Jordan Craw <jordan.craw@hurricanecommerce.com>
2025-03-28 18:37:15 +0000

    fix: site ur;

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rageycomma.github.io',
+	site: 'https://rageycomma.github.com',
 	base: 'oss-serverless-docs',
 	integrations: [
 		starlight({
